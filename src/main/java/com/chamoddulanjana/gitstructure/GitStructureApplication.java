e343b48ee5fafcf12b46426282d07fb6bede2050
ChamodDulanjana <chamodperera128@gmail.com>
2024-09-03 13:46:20 +0530

    First commit

---
+++ src/main/java/com/chamoddulanjana/gitstructure/GitStructureApplication.java
@@ -0,0 +1,13 @@
+package com.chamoddulanjana.gitstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chamoddulanjana/gitstructure/GitStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamoddulanjana.gitstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
